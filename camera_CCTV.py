68b51f0511f4347ea88018d6bf157aeb094dfa07
Minji-Sohn1 <cyzh1311@ajou.ac.kr>
2022-05-19 20:36:33 +0900

    live streaming

--- camera_CCTV.py
+++ camera_CCTV.py
@@ -25,13 +25,13 @@ class StreamingOutput(object):
 
     def write(self,buf):
         if buf.startswith(b'\xff\xd8'):
+            #New frame
             self.buffer.truncate()
-
             with self.condition():
                 self.frame=self.buffer.getvalue()
                 self.condition.notify_all()
             self.buffer.seek(0)
-            
+
         return self.buffer.write(buf)
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
